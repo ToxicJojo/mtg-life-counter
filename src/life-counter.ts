5d2408bdb9be364bec190b7ccfbbd3f143d12bdc
Johannes Thiel <hi@johannesthiel.me>
2023-06-29 23:40:37 +0200

    added ci pipeline

--- src/life-counter.ts
+++ src/life-counter.ts
@@ -1,9 +1,7 @@
 import { html } from "./html";
 
 const template = html`
-  <section
-    class="flex justify-center items-center relative bg-teal-400 h-full rounded-2xl"
-  >
+  <section class="flex justify-center items-center relative bg-teal-400 h-full rounded-2xl">
     <button id="button-plus" class="absolute top-0 w-full h-1/2"></button>
     <p id="value" class="text-9xl font-bold">20</p>
     <button id="button-minus" class="absolute bottom-0 w-full h-1/2"></button>
@@ -23,9 +21,10 @@ export default class LifeCounter extends HTMLElement {
     this.appendChild(templateElement.content.cloneNode(true));
     this.value = 20;
 
-    const plusButton = this.querySelector("#button-plus")!;
-    const minusButton = this.querySelector("#button-minus")!;
-    this.valueElement = this.querySelector<HTMLParagraphElement>("#value")!;
+    const plusButton = this.querySelector("#button-plus") as HTMLButtonElement;
+
+    const minusButton = this.querySelector("#button-minus") as HTMLButtonElement;
+    this.valueElement = this.querySelector<HTMLParagraphElement>("#value") as HTMLParagraphElement;
 
     plusButton.addEventListener("click", () => this.#plus());
     minusButton.addEventListener("click", () => this.#minus());
